0314750c283c2a78f8620a254a050bc9b2f3981c
Marc Galoyer <mgaloyer@uneak.fr>
2015-06-26 17:41:12 -0400

    fix goog map

--- Block/Component/GoogleMap.php
+++ Block/Component/GoogleMap.php
@@ -1,55 +1,53 @@
 <?php
 
-namespace Uneak\FlatSkinBundle\Block\Component;
+	namespace Uneak\FlatSkinBundle\Block\Component;
 
-use Uneak\AssetsManagerBundle\Assets\Js\AssetExternalJs;
-use Uneak\AssetsManagerBundle\Assets\Js\AssetInternalJs;
-use Uneak\BlocksManagerBundle\Blocks\Component;
+	use Uneak\AssetsManagerBundle\Assets\AssetBuilder;
+	use Uneak\AssetsManagerBundle\Assets\Js\AssetExternalJs;
+	use Uneak\AssetsManagerBundle\Assets\Js\AssetInternalJs;
+	use Uneak\BlocksManagerBundle\Blocks\Component;
 
-class GoogleMap extends Component
-{
-    protected $locationData;
+	class GoogleMap extends Component {
+		protected $locationData;
 
-    public function __construct($title = "", $locationData = array())
-    {
-        parent::__construct();
-        $this->metas->_init($locationData);
+		public function __construct($title = "", $locationData = array()) {
+			parent::__construct();
+			$this->metas->_init($locationData);
 
-        $this->template = 'UneakFlatSkinBundle:Block:Component/component_googlemap.html.twig';
+			$this->template = 'UneakFlatSkinBundle:Block:Component/component_googlemap.html.twig';
 
-    }
+		}
 
 
-	public function buildAsset(AssetBuilder $builder, $parameters) {
+		public function buildAsset(AssetBuilder $builder, $parameters) {
 
-		$builder
-			->add("google_map_js", new AssetExternalJs(), array(
-				"src" => "http://maps.google.com/maps/api/js?sensor=false&libraries=places"
-			))
-			->add("script_google_map", new AssetInternalJs(), array(
-				"src" => "UneakFlatSkinBundle:Block:GoogleMap/googlemap_script.html.twig",
-				"parameters" => array('item' => $parameters)
-			));
+			$builder
+				->add("google_map_js", new AssetExternalJs(), array(
+					"src" => "http://maps.google.com/maps/api/js?sensor=false&libraries=places"
+				))
+				->add("script_google_map", new AssetInternalJs(), array(
+					"template"   => "UneakFlatSkinBundle:Block:GoogleMap/googlemap_script.html.twig",
+					"parameters" => array('item' => $parameters)
+				));
+
+		}
+
+
+		/**
+		 * @return mixed
+		 */
+		public function getLocationData() {
+			return $this->locationData;
+		}
+
+		/**
+		 * @param mixed $locationData
+		 */
+		public function setLocationData($locationData) {
+			$this->locationData = $locationData;
+
+			return $this;
+		}
+
 
 	}
-
-
-    /**
-     * @return mixed
-     */
-    public function getLocationData()
-    {
-        return $this->locationData;
-    }
-
-    /**
-     * @param mixed $locationData
-     */
-    public function setLocationData($locationData)
-    {
-        $this->locationData = $locationData;
-        return $this;
-    }
-
-
-}
